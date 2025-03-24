b1e8c2fbac0d327ca06e9bcf437560ad08f3ffc3
marcomaddo <profmaddo@gmail.com>
2025-03-23 21:28:41 -0400

    Criação completa da classe Estoque.java

--- Estoque.java
+++ Estoque.java
@@ -1,2 +1,22 @@
+import java.util.List;
+
 public class Estoque {
+
+    private List<Produto> listarProdutos;
+
+    public List<Produto> getListarProdutos() {
+        return listarProdutos;
+    }
+
+    public void setListarProdutos(List<Produto> listarProdutos) {
+        this.listarProdutos = listarProdutos;
+    }
+
+    public void verificarDisponibilidade(Produto produto){
+        // TODO: Criar regras
+    }
+
+    public void atualizarEstoque(Produto produto){
+        // TODO: Criar regras
+    }
 }
