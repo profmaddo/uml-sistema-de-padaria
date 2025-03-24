d8d1a9fbec3aae1997f6fc543906ca50a6af5a3e
marcomaddo <profmaddo@gmail.com>
2025-03-23 21:17:06 -0400

    Criação completa da classe Pagamento.java

--- Pagamento.java
+++ Pagamento.java
@@ -1,2 +1,44 @@
 public class Pagamento {
+
+    private int idPagamento;
+    private double valor;
+    private String metodo;
+    private String status;
+
+    public int getIdPagamento() {
+        return idPagamento;
+    }
+
+    public void setIdPagamento(int idPagamento) {
+        this.idPagamento = idPagamento;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getMetodo() {
+        return metodo;
+    }
+
+    public void setMetodo(String metodo) {
+        this.metodo = metodo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void processarPagamento(){
+        // TODO: Criar regras de negócio
+    }
+
 }
