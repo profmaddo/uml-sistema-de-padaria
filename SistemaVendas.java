6bda37f292f7cf26514df9e6fab0df59dff97815
marcomaddo <profmaddo@gmail.com>
2025-03-23 21:21:49 -0400

    Criação completa da classe SistemaVendas.java

--- SistemaVendas.java
+++ SistemaVendas.java
@@ -1,2 +1,22 @@
+import java.util.List;
+
 public class SistemaVendas {
+
+    private List<Pedido> listarPedidos;
+
+    public List<Pedido> getListarPedidos() {
+        return listarPedidos;
+    }
+
+    public void setListarPedidos(List<Pedido> listarPedidos) {
+        this.listarPedidos = listarPedidos;
+    }
+
+    public void registrarVenda(Pedido pedido){
+        // TODO: Criar regras de negócio
+    }
+
+    public void gerarRelatorioVendas(){
+        // TODO: Criar regras de negócio
+    }
 }
