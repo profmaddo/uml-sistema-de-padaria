a15e5edb99466d9b112397e1a2d83ab0d0fae0d7
marcomaddo <profmaddo@gmail.com>
2025-03-23 21:36:54 -0400

    Criação completa da classe Produto.java

--- Produto.java
+++ Produto.java
@@ -1,2 +1,46 @@
 public class Produto {
+
+    private int idProduto;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    private void atualizarEstoque(int quantidade){
+        // TODO: Criar regras
+    }
+
+
+
 }
