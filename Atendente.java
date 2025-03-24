0a5092e6db974897bbecb99b43222ddac14728f6
marcomaddo <profmaddo@gmail.com>
2025-03-23 21:14:56 -0400

    Criação completa da classe Atendente.java

--- Atendente.java
+++ Atendente.java
@@ -1,2 +1,42 @@
 public class Atendente {
+
+    private String nome;
+    private int idAtendente;
+    private Pedido pedido;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdAtendente() {
+        return idAtendente;
+    }
+
+    public void setIdAtendente(int idAtendente) {
+        this.idAtendente = idAtendente;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public void registrarPedido(Pedido pedido){
+        // TODO: Criar a regra de negócio para registrar pedido
+    }
+
+    public void consultarEstoque(){
+        // TODO: Implemetar regra
+    }
+
+    public void alterarPedido(Pedido pedido){
+
+    }
 }
