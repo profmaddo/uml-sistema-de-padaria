bbe442296bedf8a92896478c68d12e73cac16d11
marcomaddo <profmaddo@gmail.com>
2025-03-23 21:27:04 -0400

    Criação completa da classe FuncionarioCaixa.java

--- FuncionarioCaixa.java
+++ FuncionarioCaixa.java
@@ -1,2 +1,30 @@
 public class FuncionarioCaixa {
+
+    private String nome;
+    private int idCaixa;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdCaixa() {
+        return idCaixa;
+    }
+
+    public void setIdCaixa(int idCaixa) {
+        this.idCaixa = idCaixa;
+    }
+
+    public void receberPagamento(Pagamento pagamento){
+        // TODO: Criar regras
+    }
+
+    public void emitirRecibo(Pedido pedido){
+        // TODO: Criar regras
+    }
+
 }
